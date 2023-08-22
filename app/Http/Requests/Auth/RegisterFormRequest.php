1224fc60f874f3cae16be4bcc60c72334efd296f
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-08-23 00:30:27 +0100

    login & register request

---
+++ app/Http/Requests/Auth/RegisterFormRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            "username" => "required|max:200",
+            "first_name" => "required|max:200",
+            "last_name" => "required|max:200",
+            "email" => "required|email|unique:users,email|max:200",
+            "password" => "required|min:6"
+        ];
+    }
+}
