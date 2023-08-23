8d024fc74f8a9b0233cbdc883c03889afc0f9370
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-08-23 20:05:37 +0100

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -79,6 +79,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapAuthRoutes()
     {
         Route::prefix('auth')
+            ->middleware("web")
             ->namespace($this->namespace)
             ->group(base_path('routes/auth.php'));
     }
