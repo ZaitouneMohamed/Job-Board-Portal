8567b4fc67ede7994d84a9f987fdb652fff66d5c
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-08-24 10:57:01 +0100

    user profile master

---
+++ resources/views/auth/profile/fournisseur/layouts/master.blade.php
@@ -0,0 +1,60 @@
+<div class="col-md-4">
+    <div class="account-information">
+        <div class="profile-thumb">
+            <img src="{{ asset('job-assets/assets/img/account.jpg') }} " alt="account holder image">
+            <h3>John Smith</h3>
+            <p>Web Developer</p>
+        </div>
+
+        <ul>
+            <li>
+                <a href="#" class="active">
+                    <i class='bx bx-user'></i>
+                    My Profile
+                </a>
+            </li>
+            <li>
+                <a href="resume.html">
+                    <i class='bx bxs-file-doc'></i>
+                    My Resume
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-briefcase'></i>
+                    Applied Job
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-envelope'></i>
+                    Messages
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-heart'></i>
+                    Saved Jobs
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-lock-alt'></i>
+                    Change Password
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-coffee-togo'></i>
+                    Delete Account
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-log-out'></i>
+                    Log Out
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
