797c0d114e7f0d78819e4690d04693cba0cbd7e5
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-08-25 21:31:08 +0100

    Create HomeController.php

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Annonce;
+use App\Models\Categorie;
+use App\Models\Company;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $topCategories = Categorie::withCount('Annonces')
+            ->orderByDesc('annonces_count')
+            ->take(8)
+            ->get();
+        $annonces = Annonce::take(6)->with('categorie')->latest()->get();
+        $companies = Company::withCount('Annonces')
+            ->orderByDesc('annonces_count')
+            ->take(4)
+            ->get();
+        return view('welcome', compact("topCategories", "annonces","companies"));
+    }
+}
