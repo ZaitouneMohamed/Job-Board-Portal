4e18753174ee155e2f8bd9807b3eba315a8d9f45
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-08-25 23:24:28 +0100

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -26,6 +26,8 @@
 		<link rel="stylesheet" href="{{ asset('job-assets/assets/css/responsive.css') }}">
         <!-- Title CSS -->
         <title>Jovie - Job Board & Portal HTML Template</title>
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         @yield("style")
         <script src="//unpkg.com/alpinejs" defer></script>
         <!-- Favicon -->
