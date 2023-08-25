885731e99ce47c1dedb589fb97bdf2b57967fbf4
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-08-25 23:48:02 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(RolesSeeder::class);
         $this->call(UsersSeeder::class);
-        \App\Models\user\Profile::factory(1)->create();
+        // \App\Models\user\Profile::factory(1)->create();
         \App\Models\user\Education::factory(5)->create();
         \App\Models\user\Experience::factory(5)->create();
         \App\Models\user\Skill::factory(15)->create();
