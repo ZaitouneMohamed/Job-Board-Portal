f8693cdc860a2b0b553de2b9fd089cb9498e7b42
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-08-26 12:49:03 +0100

    Revert "Update DatabaseSeeder.php"
    This reverts commit c784386ec8a39f7bfc8e25c50816c18633bd0bcb.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,10 +17,10 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(RolesSeeder::class);
         $this->call(UsersSeeder::class);
-        // \App\Models\user\Profile::factory(1)->create();
-        // \App\Models\user\Education::factory(5)->create();
-        // \App\Models\user\Experience::factory(5)->create();
-        // \App\Models\user\Skill::factory(15)->create();
+        \App\Models\user\Profile::factory(1)->create();
+        \App\Models\user\Education::factory(5)->create();
+        \App\Models\user\Experience::factory(5)->create();
+        \App\Models\user\Skill::factory(15)->create();
         \App\Models\Company::factory(5)->create();
         \App\Models\Categorie::factory(10)->create();
         \App\Models\Annonce::factory(20)->create();
