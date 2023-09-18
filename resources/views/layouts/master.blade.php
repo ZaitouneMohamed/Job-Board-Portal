d227187669ac69e25ad9f7f9b8146c8aa3b2fa17
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-09-18 19:08:25 +0100

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -26,7 +26,6 @@
 		<link rel="stylesheet" href="{{ asset('job-assets/assets/css/responsive.css') }}">
         <!-- Title CSS -->
         <title>Jovie - Job Board & Portal HTML Template</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         @yield("style")
@@ -74,7 +73,6 @@
 		</div>
 		<!-- Back To Top End -->
         @yield("script")
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 		<!-- jQuery first, then Bootstrap JS -->
 		<script src="{{ asset('job-assets/assets/js/jquery.min.js') }}"></script>
 		<script src="{{ asset('job-assets/assets/js/bootstrap.bundle.min.js') }}"></script>
