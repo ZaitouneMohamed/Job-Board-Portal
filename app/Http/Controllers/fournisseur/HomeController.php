6b413659d598577794509b7d74ffcc87eec97bba
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-09-18 20:51:54 +0100

    Update HomeController.php

--- app/Http/Controllers/fournisseur/HomeController.php
+++ app/Http/Controllers/fournisseur/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\fournisseur;
 
 use App\Http\Controllers\Controller;
 use App\Models\Annonce;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -11,11 +12,17 @@ class HomeController extends Controller
     function AppliedUsersOnAnnonce($id)
     {
         $annonce = Annonce::with("users")->find($id);
-        return view('pages.Users.UsersAppliedOnAnnonce',compact("annonce"));
+        return view('auth.profile.fournisseur.Users.UsersAppliedOnAnnonce', compact("annonce"));
     }
     function UsersSaveAnnonce($id)
     {
         $annonce = Annonce::with("users")->find($id);
-        return view('pages.Users.UsersSavingOnAnnonce',compact("annonce"));
+        return view('auth.profile.fournisseur.Users.UsersSavingOnAnnonce', compact("annonce"));
+    }
+    function Userprofile($username)
+    {
+        $user = User::where('username', $username)->with(["Profile", "Skills", "Education"])->get();
+        dd($user);
+        return view('auth.profile.fournisseur.Users.UserProfile', compact("user"));
     }
 }
